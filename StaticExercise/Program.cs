e74cfb025681d5e224c5ca50905d73e93f9f7f19
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-22 17:43:26 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,9 @@
+﻿using StaticExercise;
+
+double fahrenheitTempToConvert = 50;
+var celciusTemp = TempConverter.FahrenheitToCelsius(fahrenheitTempToConvert);
+Console.WriteLine($"{fahrenheitTempToConvert} degrees Fahrenheit = {celciusTemp} degrees Celcius");
+
+double celciusTempToConvert = 35;
+var fahrenheitTemp = TempConverter.CelsiusToFahrenheit(celciusTempToConvert);
+Console.WriteLine($"{celciusTempToConvert} degrees Celsius = {fahrenheitTemp} degrees Fahrenheit");
